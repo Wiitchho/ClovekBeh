4728e93ac14cf04327c65bd7209f90de30a153be
Ondřej Vinkler <vinkler.it@gmail.com>
2024-04-15 10:37:32 +0200

    Add app
    Add complete human run and counting with his tired

---
+++ ClovekBeh/Clovek.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineApp
+{
+
+    class Clovek
+    {
+        /// <summary>
+        /// Celé jméno
+        /// </summary>
+        private string jmeno;
+
+        /// <summary>
+        /// Věk
+        /// </summary>
+        private int vek;
+
+        /// <summary>
+        /// Únava
+        /// </summary>
+        private int unava = 0;
+
+        // Zde dokonči úlohu svým kódem...
+
+        public Clovek(string jmeno, int vek)
+        {
+            this.jmeno = jmeno;
+            this.vek = vek;
+        }
+
+        public int Beh(int vzdalenost)
+        {
+            if(unava < 20)
+            {
+                return unava += vzdalenost;
+            }
+            else
+            {
+                Console.WriteLine("Jsem příliš unavený");
+                return unava;
+            }
+        }
+        public int Spanek(int spanek)
+        {
+            return unava -= spanek;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{jmeno}({vek})");
+        }
+    }
+
+}
---
+++ ClovekBeh/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Clovek karel = new Clovek("Karel Nový", 25);
+
+            // Zde dokonči úlohu svým kódem...
+            Console.WriteLine(karel);
+            karel.Beh(10);
+            karel.Beh(10);
+            karel.Spanek(0);
+            karel.Beh(10);
+            Console.ReadKey();
+        }
+    }
+}
